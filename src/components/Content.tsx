8f462114b3795de0d38b1931048c5ab682e9c2d9
Leonardo Salgueiro <leonardosalgueiro@live.com>
2023-04-02 13:53:27 -0700

    prettier run

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -2,21 +2,21 @@ import AnimalsChecklist from './AnimalsChecklist'
 import PeopleChecklist from './PeopleChecklist'
 
 const Content = () => {
-  return (
-    <main className="flex grow flex-col w-240 m-6">
-      <h2 className="text-center text-xl">
-        Love Level 1 - Love's Awakening - Total Love 0
-      </h2>
-      <div className="flex">
-        <div className="grow w-3/6 m-4">
-          <AnimalsChecklist />
-        </div>
-        <div className="grow w-3/6 m-4">
-          <PeopleChecklist />
-        </div>
-      </div>
-    </main>
-  )
+    return (
+        <main className="flex grow flex-col w-240 m-6">
+            <h2 className="text-center text-xl">
+                Love Level 1 - Love's Awakening - Total Love 0
+            </h2>
+            <div className="flex">
+                <div className="grow w-3/6 m-4">
+                    <AnimalsChecklist />
+                </div>
+                <div className="grow w-3/6 m-4">
+                    <PeopleChecklist />
+                </div>
+            </div>
+        </main>
+    )
 }
 
 export default Content
--- src/components/PeopleChecklist.tsx
+++ src/components/PeopleChecklist.tsx
@@ -1,44 +1,44 @@
 import data from '../data/moon_data.json'
 
 const PeopleChecklist = () => {
-  const { people } = data
+    const { people } = data
 
-  return (
-    <div>
-      <h2 className="text-center text-3xl">People</h2>
-      <div className="bg-black m-4">
-        <ul>
-          {people.map(({ name, wiki }) => (
-            <li key={name} className="m-3 ">
-              <div className="form-control w-52">
-                <label
-                  className="cursor-pointer label justify-start whitespace-nowrap"
-                  htmlFor={name}
-                >
-                  <input
-                    id={name}
-                    type="checkbox"
-                    name="progress"
-                    data-key={name}
-                    className="checkbox checkbox-secondary checkbox-md "
-                  />
-                  <span className="label-text ml-4 text-xl">
-                    <a
-                      href={wiki}
-                      target="_blank"
-                      className="underline decoration-1 underline-offset-2"
-                    >
-                      {name}
-                    </a>
-                  </span>
-                </label>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  )
+    return (
+        <div>
+            <h2 className="text-center text-3xl">People</h2>
+            <div className="bg-black m-4">
+                <ul>
+                    {people.map(({ name, wiki }) => (
+                        <li key={name} className="m-3 ">
+                            <div className="form-control w-52">
+                                <label
+                                    className="cursor-pointer label justify-start whitespace-nowrap"
+                                    htmlFor={name}
+                                >
+                                    <input
+                                        id={name}
+                                        type="checkbox"
+                                        name="progress"
+                                        data-key={name}
+                                        className="checkbox checkbox-secondary checkbox-md "
+                                    />
+                                    <span className="label-text ml-4 text-xl">
+                                        <a
+                                            href={wiki}
+                                            target="_blank"
+                                            className="underline decoration-1 underline-offset-2"
+                                        >
+                                            {name}
+                                        </a>
+                                    </span>
+                                </label>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 export default PeopleChecklist
