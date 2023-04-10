74aafa8ddd0ab162800efb5fa565199fe4594efd
Teryn <terynalves@gmail.com>
2023-04-10 13:02:37 -0700

    fix: changed footer order and align

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,12 @@ import { VscGithub } from 'react-icons/vsc'
 
 const Footer = () => {
   return (
-    <footer className="flex grow-0 flex-col items-center bg-accent p-2">
+    <footer className="flex grow-0 flex-col items-center bg-accent p-2 text-center">
+      <div>
+        <Link href="https://github.com/dev0T/moon-RPG-Love-Checklist">
+          <VscGithub className="p-1" size={35} />
+        </Link>
+      </div>
       <div>
         This page is a fan project. It is not affiliated with Onion Games.
       </div>
@@ -11,11 +16,6 @@ const Footer = () => {
       <div>
         Made by <Link href="https://github.com/dev0T">dev0T</Link> &{' '}
         <Link href="https://github.com/salguerooo">salguerooo</Link>
-      </div>
-      <div>
-        <Link href="https://github.com/dev0T/moon-RPG-Love-Checklist">
-          <VscGithub className="m-2" size={25} />
-        </Link>
       </div>
     </footer>
   )
